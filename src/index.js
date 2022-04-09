745ea12060555a78fd9ff8869b8a990601cf322a
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-04-08 18:41:19 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as IPFS from 'ipfs-core';
+export function sayHelloWorld() {
+    return `Hello ${IPFS}`;
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as IPFS from 'ipfs-core';
+
+export function sayHelloWorld() {
+  return `Hello ${IPFS}`;
+}
