97ff358c6a822579cf165d775482e2fa6677bf81
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-04-11 14:42:08 -0700

    use same typescript version as aegir

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 // this builds
 import * as IPFSUtils from "ipfs-utils";
+// import globSourceImport from "ipfs-utils/src/files/glob-source.js";
+import * as fs from "fs";
+import * as path from "path";
 
 /*
 This errors with:
@@ -12,5 +15,9 @@ node_modules/ipfs-utils/dist/src/files/glob-source.d.ts:10:14 - error TS2503: Ca
 import * as IPFSCore from "ipfs-core";
 
 export function sayHelloWorld() {
-  return `Hello ${IPFSUtils} ${IPFSCore}`;
+  return `Hello ${IPFSUtils}`;
+}
+
+export function createFsReadStream() {
+  return fs.createReadStream(path.join(__dirname, "../README.md"));
 }
